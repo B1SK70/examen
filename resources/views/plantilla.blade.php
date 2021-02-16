cdd297f654022a3c94d598855eb38af7789c4a2e
arnau <arnaubisqui70@gmail.com>
2021-02-16 19:33:42 +0100

    apartado3

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -79,15 +79,17 @@
 <!-- Begin page content -->
 <main class="flex-shrink-0">
   <div class="container">
-    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-    <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-    <p>Back to <a href="/docs/5.0/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+    <h1 class="mt-5">@yield('headder');</h1>
+    <p class="lead">@yield('section')</p>
+
   </div>
 </main>
 
 <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted">
+        @yield('footer');
+    </span>
   </div>
 </footer>
 
