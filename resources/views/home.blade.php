2109c4604a97765dffbac9ec7d60ec6c850db45d
arnau <arnaubisqui70@gmail.com>
2021-02-16 20:04:03 +0100

    apartado6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,10 @@
 @endsection
 
 @section('contenido')
-    Bienvenido!    
+    Bienvenido! 
+    Esta es mi pagina web express
 
+    <a class="img-fluid" href="https://images.pexels.com/photos/357573/pexels-photo-357573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></a>
 
 
 @endsection
