0683b433446a8d64958c18d59141c21d818c701f
arnau <arnaubisqui70@gmail.com>
2021-02-16 20:36:24 +0100

    apartado3 Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     Bienvenido! 
     Esta es mi pagina web express
 
-    <a class="img-fluid" href="https://images.pexels.com/photos/357573/pexels-photo-357573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></a>
+    <img src="{{ URL::to('https://images.pexels.com/photos/3837278/pexels-photo-3837278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') }}"
 
 
 @endsection
