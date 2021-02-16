76f98e4f8a0765493b83f55cca7e859686aa4a9d
arnau <arnaubisqui70@gmail.com>
2021-02-16 19:48:35 +0100

    coomit5

---
+++ resources/views/seeders.blade.php
@@ -0,0 +1,17 @@
+@extends('plantilla');
+
+@section('header')
+    
+    <ul>
+        <li><a>Home</a></li>
+        <li><a>Migraciones</a></li>
+        <li><a>Seeders</a></li>
+        <li><a>Mail</a></li>
+    </ul>
+    
+@endsection
+
+
+@section('footer')
+    Arnau Bisquerra Abella 2GS-DAW 16/02/21
+@endsection
